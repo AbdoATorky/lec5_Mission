af9e9f649f8795bc1608d6652f6be0766851194a
AbdoATorky <abdalsalamtorky@gmail.com>
2026-03-11 18:14:15 +0200

    Implement activity navigation and update UI components
    - Add logic to `SecondActivity.kt` to navigate to `ThirdActivity`.
    - Add logic to `ThirdActivity.kt` to navigate back to `MainActivity`.
    - Populate `activity_third.xml` with a centered "3" TextView and a "Start Over" Button.
    - Update background colors in `activity_main.xml` and `activity_third.xml`.

--- app/src/main/java/com/abs/lec5mission/SecondActivity.kt
+++ app/src/main/java/com/abs/lec5mission/SecondActivity.kt
@@ -1,6 +1,8 @@
 package com.abs.lec5mission
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +19,11 @@ class SecondActivity : AppCompatActivity() {
             insets
         }
 
+        val btn = findViewById<Button>(R.id.btn)
+        btn.setOnClickListener {
+            val i = Intent(this, ThirdActivity::class.java)
+            startActivity(i)
 
+        }
     }
 }
--- app/src/main/java/com/abs/lec5mission/ThirdActivity.kt
+++ app/src/main/java/com/abs/lec5mission/ThirdActivity.kt
@@ -1,6 +1,8 @@
 package com.abs.lec5mission
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class ThirdActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btn = findViewById<Button>(R.id.btn)
+        btn.setOnClickListener {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+
         }
     }
 }
