8acacae13113a44af7022dbdfb8aaaa28d57da4f
AbdoATorky <abdalsalamtorky@gmail.com>
2026-03-11 18:01:30 +0200

    Add ThirdActivity and update SecondActivity layout
    - Create `ThirdActivity.kt` and its corresponding layout `activity_third.xml`.
    - Register `ThirdActivity` in `AndroidManifest.xml`.
    - Update `activity_second.xml` with a green background, a TextView "2", and a "Show Blue" Button.

--- app/src/main/java/com/abs/lec5mission/ThirdActivity.kt
+++ app/src/main/java/com/abs/lec5mission/ThirdActivity.kt
@@ -1,30 +1,20 @@
 package com.abs.lec5mission
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_third)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val btn = findViewById<Button>(R.id.btn)
-        btn.setOnClickListener {
-            val i = Intent(this, SecondActivity::class.java)
-            startActivity(i)
-
-        }
-
     }
 }
